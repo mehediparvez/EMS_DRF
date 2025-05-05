8945e49389e6605f0ccefb47c5d8dd9ead9a0e63
MD Mehedi Hasan Parvez <mehediparvez11908015@gmail.com>
2025-05-05 21:34:52 +0600

    feat(api): add user serializers

---
+++ apps/users/serializers/__init__.py
@@ -0,0 +1,2 @@
+from apps.users.serializers.user_serializer import UserSerializer, UserDetailSerializer
+from apps.users.serializers.employer_serializer import EmployerSerializer
---
+++ apps/users/serializers/user_serializer.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'name', 'password', 'password2')
+        extra_kwargs = {
+            'name': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'name', 'date_joined')
+        read_only_fields = ('id', 'email', 'date_joined')
